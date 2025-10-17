6f8da0985769ea7e6d14851e946d3d24a28673e3
bhtrwhyou <bahterawahyualmunirr@gmail.com>
2025-10-17 10:20:58 +0700

    Add files via upload

---
+++ revisitotal/script.js
@@ -0,0 +1,8 @@
+
+document.querySelector(".btn-lampung").addEventListener("click", () => {
+  alert("Selamat menjelajahi keindahan Lampung! 🌴✨");
+});
+
+document.querySelector(".btn-lampung.green").addEventListener("click", () => {
+  alert("Panduan wisata Lampung berhasil diunduh! 📖");
+});
